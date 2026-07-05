9b20f68a9a0a39530efb66dea6538dddf1be9cad
3050Inter <jjssbbp@gmail.com>
2026-07-06 05:40:43 +0900

    초기화

--- lib/constants.ts
+++
@@ -1,5 +0,0 @@
-import type { PaymentMethod } from "./types";
-
-export const PAYMENT_METHODS: PaymentMethod[] = ["카드", "현금", "계좌"];
-export const EXPENSE_CATEGORIES = ["주류매입", "안주재료", "인건비", "월세", "공과금", "비품", "기타지출"];
-export const API_URL = "https://script.google.com/macros/s/AKfycbwFp7piFkt36NMRF9PNAyM-8j5MSfJs5o0mya1oH9-Q_cUV-QZvaj7-KQ0-PQvCUfK6iQ/exec";
